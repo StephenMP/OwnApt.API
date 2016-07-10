60080fe25c0bef0565e4191ed5684a4b9ef1f4fc
sporter <sporter@ownapt.com>
2016-07-10 09:16:39 -0600

    Updated object extensions

--- src/Api/Extensions/ObjectExtensions.cs
+++ src/Api/Extensions/ObjectExtensions.cs
@@ -9,12 +9,7 @@ namespace OwnApt.Api.Extensions
 
         public static int GetHashCodeSafe(this object obj)
         {
-            if (obj == null)
-            {
-                return 0;
-            }
-
-            return obj.GetHashCode();
+            return obj == null ? 0 : obj.GetHashCode();
         }
 
         public static int GetHashCodeSafe<T>(this IEnumerable<T> enumerable)
