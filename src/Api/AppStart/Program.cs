054bff0e64deb33e5fc98cbfc42427e0597d831f
sporter <sporter@ownapt.com>
2016-08-29 21:08:05 -0600

    19 : Try with 0.0.0.0 as url

--- src/Api/AppStart/Program.cs
+++ src/Api/AppStart/Program.cs
@@ -17,6 +17,7 @@ namespace Api
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseIISIntegration()
                 .UseStartup<Startup>()
+                .UseUrls("http://0.0.0.0:5000")
                 .Build();
 
             host.Run();
